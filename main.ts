797cc9b58d848816f5df0a762ed4ff45759ebb64
ROBOTV15 <124434857+ROBOTV15@users.noreply.github.com>
2023-02-04 16:27:58 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 bitbot.ledRainbow()
 bitbot.select_model(BBModel.Classic)
-bitbot.goms(BBDirection.Forward, 60, 1000)
-bitbot.rotatems(BBRobotDirection.Left, 60, 1000)
+bitbot.goms(BBDirection.Forward, 60, 500)
+bitbot.rotatems(BBRobotDirection.Left, 60, 500)
 bitbot.rotatems(BBRobotDirection.Right, 60, 1000)
 bitbot.goms(BBDirection.Forward, 60, 2000)
